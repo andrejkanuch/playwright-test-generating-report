77976e471511080c81d1bf66749853eac093867e
Andrej Kanuch <kanuchandrej@gmail.com>
2023-04-07 16:19:19 +0200

    3

--- merge-reports.ts
+++ merge-reports.ts
@@ -1,6 +1,9 @@
 const { mergeHTMLReports } = require("playwright-merge-html-reports");
 
-mergeHTMLReports([
-  process.cwd() + "/playwright-report-1",
-  process.cwd() + "/playwright-report-2",
-]);
+mergeHTMLReports(
+  [
+    process.cwd() + "/playwright-report-1",
+    process.cwd() + "/playwright-report-2",
+  ],
+  { outputBasePath: process.cwd() + "/playwright-report-folder" }
+);
